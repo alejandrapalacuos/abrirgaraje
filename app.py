4fa76f5be753214003cd17b332cb88f8f8212ad2
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-11-14 10:09:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,8 @@ st.write("Versión de Python:", platform.python_version())
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de gestos!")
+st.title("Abre el garaje!")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
-st.image(image, width=500)
-with st.sidebar:
-    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar. ")
 img_file_buffer = st.camera_input("Toma una foto ;)")
 
 if img_file_buffer is not None:
